ac888a9410f2e85c60672331e9dcccfcf0a5da66
Anton Kuznetsov <lukarek@Antons-MacBook-Pro.local>
2021-05-23 00:46:28 +0400

    main.js edit

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log('hey guys');
+
+if (console.everything === undefined)
+{
+    const names = [];
+
+    console.defaultError = console.error.bind(console);
+    console.error = function(){
+        names.push({"type":"error", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
+        console.defaultError.apply(console, arguments);
+    }
+
+    names.push('1');
+
+    console
+}
